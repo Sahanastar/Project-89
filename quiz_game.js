cbda8b84826c36a104440dabcf2a8dd0edecceb4
Sahanastar <81565078+Sahanastar@users.noreply.github.com>
2021-09-25 07:45:31 +0530

    Delete quiz_game.js

--- quiz_game.js
+++
@@ -1,9 +0,0 @@
-   function addUser(){
-       player_1= document.getElementById("player1_name").value;
-       player_2= document.getElementById("player2_name").value;
-
-       localStorage.setItem("player1_name",player_1);
-       localStorage.setItem("player2_name",player_2);
-
-       window.location = "ADV_90.html";
-   }     
